8c4f4fb9d8c0ca598e8143259951fddaf80ba487
Sofia Protsiv <prochivsofia@gmail.com>
2021-11-03 10:16:19 -0400

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,6 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
 const imgContainer = document.querySelector('.gallery');
 let galleryMarkup = createMarkup(galleryItems);
 
